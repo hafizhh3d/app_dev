aef89a53a788c10158bd814c57bf2b75be6e1248
Hafizh Herti <hafizh.h3d@gmail.com>
2021-11-25 22:45:59 +0800

    Fixed product_model.dart import

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:/helper/hexColor_extension.dart';
+import 'package:Digati/helper/hexColor_extension.dart';
 
 class ProductModel {
   late String name, image, description, size, price, productId, category;
