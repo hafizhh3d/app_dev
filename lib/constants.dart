9d12374f7c1eaf541ea4dbfa307b7d2edcb341dd
leonna <67146445+leonnahart@users.noreply.github.com>
2021-11-26 09:24:58 +0800

    accept constants changes

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,3 @@
 import 'package:flutter/material.dart';
 
-<<<<<<< HEAD
-const primaryColor = Color.fromRGBO(0, 171, 240, 1);
-=======
 const primaryColor = Color.fromARGB(255, 248, 74, 43);
->>>>>>> main
