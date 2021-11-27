b0fb0f494cfbe0d194d38d2defae656146b0c8d2
Hafizh Herti <hafizh.h3d@gmail.com>
2021-11-28 04:46:13 +0800

    Updated pubspec.yaml

--- lib/model/product_model.dart
+++ lib/model/product_model.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:Digati/helper/hexColor_extension.dart';
+import 'package:digati/helper/hexColor_extension.dart';
 
 class ProductModel {
   late String name, image, description, size, price, productId, category;
