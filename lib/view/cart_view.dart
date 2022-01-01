c453496641a484c72883a76c483ea119067ea849
Hafizh Herti <hafizh.h3d@gmail.com>
2022-01-02 01:17:20 +0800

    code cleanup

--- lib/view/cart_view.dart
+++ lib/view/cart_view.dart
@@ -1,4 +1,3 @@
-import 'package:digati/view/checkout_payment_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
